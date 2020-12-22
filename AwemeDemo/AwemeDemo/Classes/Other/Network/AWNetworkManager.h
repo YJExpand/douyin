6087d79322810fe810661393d3f39ec50413e2f5
YJExpand <yaoweijie@meiqijiacheng.com>
2020-12-22 11:32:26 +0800

    导入第三方库

---
+++ AwemeDemo/AwemeDemo/Classes/Other/Network/AWNetworkManager.h
@@ -0,0 +1,16 @@
+//
+//  AWNetworkManager.h
+//  AwemeDemo
+//
+//  Created by YJExpand on 2020/12/22.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AWNetworkManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ AwemeDemo/AwemeDemo/Classes/Other/Network/AWNetworkManager.m
@@ -0,0 +1,12 @@
+//
+//  AWNetworkManager.m
+//  AwemeDemo
+//
+//  Created by YJExpand on 2020/12/22.
+//
+
+#import "AWNetworkManager.h"
+
+@implementation AWNetworkManager
+
+@end
